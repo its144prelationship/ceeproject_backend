5c588c8b7f865674ab7b9922f08a709dcbdf1800
Chanatip Pattanapen <lovels2906@gmail.com>
2023-04-30 20:42:35 +0700

    fix: invite route

--- routes.js
+++ routes.js
@@ -3,11 +3,10 @@ const controller = require("./controllers");
 
 const router = express.Router();
 
-router.get("/",controller.getStudent);
-router.post("/",controller.createEvent)
-router.delete("/",controller.deleteUserEvent);
-router.post("/",controller.createInvitation); //อันนี้ไม่แน่ใจ method นะ
-router.delete("/invite",controller.deleteInvitation)
-
+router.get("/", controller.getStudent);
+router.post("/", controller.createEvent);
+router.delete("/", controller.deleteUserEvent);
+router.post("/invite", controller.createInvitation);
+router.delete("/invite", controller.deleteInvitation);
 
 module.exports = router;
