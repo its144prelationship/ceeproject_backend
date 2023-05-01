cee37fa12a637731ba321d322518d50b63a50679
Tinnaung <6430129921@student.chula.ac.th>
2023-05-01 22:55:05 +0700

    change url

--- controllers.js
+++ controllers.js
@@ -74,7 +74,7 @@ exports.accessToken = (req, res) => {
           // console.log(req.session);
           if (token) {
             res.writeHead(302, {
-              Location: `http://${process.env.frontendIPAddress}/home.html`,
+              Location: `http://${process.env.frontendIPAddress}/component/calendar.html`,
             });
             res.end();
           }
