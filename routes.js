ccbd554d6cfe792a048e86c99d42f2fa45df6c76
Chanatip Pattanapen <lovels2906@gmail.com>
2023-05-01 22:34:26 +0700

    fix: route

--- routes.js
+++ routes.js
@@ -3,10 +3,12 @@ const controller = require("./controllers");
 
 const router = express.Router();
 
-router.get("/",controller.getStudent);
-router.post("/",controller.createEvent)
-router.delete("/",controller.deleteUserEvent);
-router.post("/",controller.createUserEvent);
-router.delete("/invite",controller.deleteInvitation)
+router.get("/auth_app", controller.authApp);
+router.get("/access_token", controller.accessToken);
+router.get("/", controller.getStudent);
+router.post("/", controller.createEvent);
+router.delete("/", controller.deleteUserEvent);
+router.post("/", controller.createUserEvent);
+router.delete("/invite", controller.deleteInvitation);
 
 module.exports = router;
