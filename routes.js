ba47a0e0053264067c07e4722b46b8378931cfe0
Chanatip Pattanapen <lovels2906@gmail.com>
2023-05-03 02:42:04 +0700

    fix: route

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ router.get("/courseville/access_token", controller.accessToken);
 router.get("/", controller.getStudent);
 router.post("/", controller.createEvent);
 router.delete("/", controller.deleteUserEvent);
-router.post("/", controller.createUserEvent);
+router.post("/invite", controller.createUserEvent);
 router.delete("/invite", controller.deleteInvitation);
 router.get("/logout", controller.logout);
 module.exports = router;
